d9b3ed937a0d0d1efa4a4ed52329d921558aff2e
seyityahya <seyityahya05@gmail.com>
2023-03-17 18:10:25 +0300

    admin panalinde ürünlerin listelenmesi

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./reset.css";
+import "antd/dist/antd";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
--- frontend/src/pages/Admin/AdminProducts/index.js
+++ frontend/src/pages/Admin/AdminProducts/index.js
@@ -1,9 +1,49 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 import "../style.css";
+import { useQuery } from "react-query";
+import { fetchProductList } from "../../../api";
+import { Table } from "antd";
 
 function AdminProducts() {
+  const { isLoading, isError, data, error } = useQuery(
+    "admin:products",
+    fetchProductList
+  );
+  const columns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+    },
+    {
+      title: "Created At",
+      dataIndex: "createdAt",
+      key: "createdAt",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (text, record) => (
+        <>
+          <Link to={`/admin/products/${record._id}`}>Edit</Link>
+        </>
+      ),
+    },
+  ];
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+  console.log(data);
   return (
     <div>
       <nav>
@@ -19,7 +59,13 @@ function AdminProducts() {
           </li>
         </ul>
       </nav>
-      <Box mt={10}>Products</Box>
+      <Box mt={10}>
+        <Text fontSize="2xl" p="5">
+          Products
+        </Text>
+
+        <Table dataSource={data} columns={columns} rowKey="_id" />
+      </Box>
     </div>
   );
 }
