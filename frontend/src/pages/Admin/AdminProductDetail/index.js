5b4382d7025b5530eb8327c842fdc11d1600d740
seyityahya <seyityahya05@gmail.com>
2023-03-19 01:58:50 +0300

    ürün güncelleme bitti

--- frontend/src/pages/Admin/AdminProductDetail/index.js
+++ frontend/src/pages/Admin/AdminProductDetail/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { fetchProduct } from "../../../api";
+import { fetchProduct, updateProduct } from "../../../api";
 import { useQuery } from "react-query";
-import { Box, Text } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Text,
+  Input,
+  Textarea,
+  Button,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import "../style.css";
-import { Formik } from "formik";
+import { Formik, FieldArray } from "formik";
+import validationSchema from "./validations";
+import { message } from "antd";
+
 function AdminProductDetail() {
   const { product_id } = useParams();
 
@@ -19,8 +30,21 @@ function AdminProductDetail() {
   if (isError) {
     return <div>Error {error.message}</div>;
   }
-  const handleSubmit = () => {
+  const handleSubmit = async (values, bag) => {
     console.log("submitted");
+    message.loading({ content: "Loading... ", key: "product_update" });
+
+    try {
+      await updateProduct(values, product_id);
+
+      message.success({
+        content: "The product successfully updates",
+        key: "product_update",
+        duration: 2,
+      });
+    } catch (e) {
+      message.error("the product does not updated.");
+    }
   };
   return (
     <div>
@@ -46,8 +70,120 @@ function AdminProductDetail() {
             price: data.price,
             photos: data.photos,
           }}
+          validationSchema={validationSchema}
           onSubmit={handleSubmit}
-        ></Formik>
+        >
+          {({
+            handleSubmit,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            values,
+            isSubmitting,
+          }) => (
+            <>
+              <Box>
+                <Box my={5} textAlign="left">
+                  <form onSubmit={handleSubmit}>
+                    <FormControl>
+                      <FormLabel>Title</FormLabel>
+                      <Input
+                        name="title"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.title}
+                        disabled={isSubmitting}
+                        isInvalid={touched.title && errors.title}
+                      />
+                      {touched.title && errors.title && (
+                        <Text mt={2} color="red.500">
+                          {errors.title}
+                        </Text>
+                      )}
+                    </FormControl>
+                    <FormControl mt={4}>
+                      <FormLabel>Description</FormLabel>
+                      <Textarea
+                        name="description"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.description}
+                        disabled={isSubmitting}
+                        isInvalid={touched.description && errors.description}
+                      />
+                      {touched.description && errors.description && (
+                        <Text mt={2} color="red.500">
+                          {errors.description}
+                        </Text>
+                      )}
+                    </FormControl>
+                    <FormControl mt={4}>
+                      <FormLabel>Price</FormLabel>
+                      <Input
+                        name="price"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.price}
+                        disabled={isSubmitting}
+                        isInvalid={touched.description && errors.description}
+                      />
+                      {touched.price && errors.price && (
+                        <Text mt={2} color="red.500">
+                          {errors.price}
+                        </Text>
+                      )}
+                    </FormControl>
+                    <FormControl mt={4}>
+                      <FormLabel>Photos</FormLabel>
+                      <FieldArray
+                        name="photos"
+                        render={(arrayHelpers) => (
+                          <div>
+                            {values.photos &&
+                              values.photos.map((photo, index) => (
+                                <div key={index}>
+                                  <Input
+                                    name={`photos.${index}`}
+                                    value={photo}
+                                    disabled={isSubmitting}
+                                    onChange={handleChange}
+                                    width="90%"
+                                  />
+                                  <Button
+                                    ml="4"
+                                    type="button"
+                                    colorScheme="red"
+                                    onClick={() => arrayHelpers.remove(index)}
+                                  >
+                                    Remove
+                                  </Button>
+                                </div>
+                              ))}
+                            <Button
+                              mt="5"
+                              onClick={() => arrayHelpers.push("")}
+                            >
+                              Add a Photo
+                            </Button>
+                          </div>
+                        )}
+                      />
+                    </FormControl>
+                    <Button
+                      mt={4}
+                      width="full"
+                      type="submit"
+                      isLoading={isSubmitting}
+                    >
+                      Update
+                    </Button>
+                  </form>
+                </Box>
+              </Box>
+            </>
+          )}
+        </Formik>
       </Box>
     </div>
   );
